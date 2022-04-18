25fa3de4b15a4457a8c671b9e5ed226f49f50fa6
Chom642 <89278032+Chom642@users.noreply.github.com>
2022-04-17 23:48:09 -0700

    TT3 code

--- model.py
+++ model.py
@@ -1,9 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from flask_migrate import Migrate
-from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-
 from __init__ import app
 
 # Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
@@ -23,7 +21,7 @@ Migrate(app, db)
 # -- a.) db.Model is like an inner layer of the onion in ORM
 # -- b.) Users represents data we want to store, something that is built on db.Model
 # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
-class Users(UserMixin, db.Model):
+class Users(db.Model):
     # define the Users schema
     userID = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=False, nullable=False)
@@ -35,7 +33,7 @@ class Users(UserMixin, db.Model):
     def __init__(self, name, email, password, phone):
         self.name = name
         self.email = email
-        self.set_password(password)
+        self.password = password
         self.phone = phone
 
     # CRUD create/add a new record to the table
@@ -80,23 +78,9 @@ class Users(UserMixin, db.Model):
         db.session.delete(self)
         db.session.commit()
         return None
-    # required for login_user, overrides id (login_user default) to implemented userID
-    # The method get_id() must return a str that uniquely identifies this user, and can be used to load the user
-    # from the user_loader callback.
 
-    def get_id(self):
-        return self.userID
 
-    def set_password(self, password):
-        """Create hashed password."""
-        self.password = generate_password_hash(password, method='sha256')
-
-    # check password to check versus encrypted password
-    def is_password_match(self, password):
-        """Check hashed password."""
-        result = check_password_hash(self.password, password)
-        return result
-
+"""Database Creation and Testing section"""
 
 
 def model_tester():
@@ -132,12 +116,39 @@ def model_printer():
     for row in result:
         print(row)
 
-# The class that you use to represent users needs to implement these properties and methods:
-# is_authenticated, is_active, is_anonymous, get_id()
-# To make implementing a user class easier, you can inherit from UserMixin, which provides default implementations
-# for all of these properties and methods.
+ # set password method is used to create encrypted password
+    def set_password(self, password):
+        """Create hashed password."""
+        * Procedural Abstraction
+        self.password = generate_password_hash(password, method='sha256')
 
-# Users DB is a collection Data Structure
+    # check password to check versus encrypted password
+    def is_password_match(self, password):
+        """Check hashed password."""
+        result = check_password_hash(self.password, password)
+        return result
+
+#The class that you use to represent users needs to implement these properties and methods:  
+#is_authenticated, is_active, is_anonymous, get_id()
+#To make implementing a user class easier, you can inherit from UserMixin, which provides default implementations  
+#for all of these properties and methods.
+
+from flask_login import UserMixin
+#Users DB is a collection Data Structure
+class Users(UserMixin, db.Model):
+    # define the Users schema
+    userID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=False, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), unique=False, nullable=False)
+    phone = db.Column(db.String(255), unique=False, nullable=False)
+
+# required for login_user, overrides id (login_user default) to implemented userID
+# The method get_id() must return a str that uniquely identifies this user, and can be used to load the user from the user_loader callback.
+def get_id(self):
+    return self.userID
+
+
 
 
 if __name__ == "__main__":
